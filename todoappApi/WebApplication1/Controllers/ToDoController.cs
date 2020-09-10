5ba165d3ff0e0a8d201702c12a4e96b2bc39147c
ApolloEstrella <34788252+ApolloEstrella@users.noreply.github.com>
2020-09-10 23:06:45 +0800

    Initial Commit

---
+++ todoappApi/WebApplication1/Controllers/ToDoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<ToDo> list = new List<ToDo>();
+            list.Add(new ToDo { Id = 1, Title = "abc", IsCompleted = true });
+            list.Add(new ToDo { Id = 2, Title = "def", IsCompleted = false });
+            list.Add(new ToDo { Id = 3, Title = "adsf", IsCompleted = true });
+            list.Add(new ToDo { Id = 4, Title = "nmfd", IsCompleted = false });
+
+            var y = Ok(JsonSerializer.Serialize(list));
+            return y;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] ToDo toDo)
+        {
+            return Ok(JsonSerializer.Serialize(toDo));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            return Ok(id);
+        }
+    }
+
+
+
+    [Bind("Id,Title,IsCompleted")]
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+
+}
+
---
+++ todoappApi/WebApplication1/Startup.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        public void ConfigureServices(IServiceCollection services)
+        {
+            /*services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });*/
+            services.AddCors(options => options.AddPolicy("Cors", builder =>
+            {
+                builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            }));
+            services.AddMvc();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseMvcWithDefaultRoute();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors("Cors");
+
+            app.UseCors(options => options.AllowAnyOrigin());
+
+            app.UseCors(options => options.AllowAnyMethod());
+
+            app.UseCors(options => options.AllowAnyHeader());
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
